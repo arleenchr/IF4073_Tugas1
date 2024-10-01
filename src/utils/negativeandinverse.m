b1c10036cea48fe827bcc280e6396dc59967091d
kevinjohn01 <16521168@std.stei.itb.ac.id>
2024-10-01 16:01:21 +0700

    feat: negative and inverse negative

---
+++ src/utils/negativeandinverse.m
@@ -0,0 +1,31 @@
+image = imread('images/peppers512warna.bmp');
+%imshow(image);
+negative_image = negative(image);
+invers_negative = negative(negative_image);
+imshow(negative_image);
+%imshow(invers_negative);
+
+function output_image = negative(image)
+    [rows, cols, color_channels] = size(image);
+    output_image = zeros(rows, cols, 'uint8');
+
+    for i = 1:rows
+        for j = 1:rows
+            for k = 1:color_channels
+                temp = 255 - image(i,j,k);
+
+                % Clipping
+                 if temp < 0
+                    output_image(i,j,k) = 0;
+                elseif temp > 255
+                    output_image(i,j,k) = 255;
+                else
+                    output_image(i,j,k) = uint8(temp);
+                end
+            end
+        end
+    end
+end
+
+
+    
