e12caa79bb803a1f445196516c4252cfd2f86286
Arleen <arleen@ecomatcher.com>
2024-10-03 14:45:27 +0700

    fix: histogram equalization for colored images

--- src/utils/compute_equalization.m
+++ src/utils/compute_equalization.m
@@ -1,7 +1,7 @@
 function hist_eq = compute_equalization(image)
     hist_frequency = compute_histogram(image);
     
-    cdf = cumsum(hist_frequency) / numel(image); 
+    cdf = cumsum(hist_frequency) / numel(image) * size(image,3); 
   
     hist_eq = uint8(floor(cdf * 255));
 end
